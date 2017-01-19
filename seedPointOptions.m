57413957f66f78a4696aa5ee13fe4c61625722f8
James Kapaldo <james.kapaldo@gmail.com>
2017-01-19 14:25:45 -0500

    Edit available options and rename
    Segmentation options were removed from the class and two new options
    were added for seed point locations: padding size, max potential value

--- seedPointOptions.m
+++ seedPointOptions.m
@@ -1,21 +1,12 @@
-classdef declumpOptions
-% DECLUMPOPTIONS  Set options needed for declumping.
+classdef seedPointOptions
+% SEEDPOINTOPTIONS  Set options needed for computing seed point locations.
 %
 % Input can be structure array or parameter value pairs. Options not set
 % will be given default values. To see the default values, look at the
 % output with no inputs:
-%   defaultValues = setDeclumpOptions()
+%   defaultValues = seedPointOptions()
 %
-% declumpOptions Properties:
-%
-% Max_Radius - The maximum distance a boundry vertex can be from a center
-%   to be assigned to it.
-%   (0, Inf) , [pixels]
-%
-% Min_Angle - The minimum *dot product* between the line formed between a
-%   boundry vertex and a center and the unit normal (pointing inward) at 
-%   the boundary vertex. -- The angle could be found by acos(Min_Angle).
-%   [-1, 1] , [unitless]
+% seedPointOptions Properties:
 %
 % Wigner_Seitz_Radius - The effective size of each particle in the
 %   simulation. This sets the density of the particles used. The
@@ -32,6 +23,17 @@ classdef declumpOptions
 % Potential_Extent - The radius at which the potential goes from attractive
 %   to repulsive.
 %   (0, Inf) & > Potential_Minimum_Location , [pixels]
+%
+% Potential_Padding_Size - The size of the padding to apply to an object
+%   mask. This is important so that the potential is defined some distance
+%   away from the object when running the simulation.
+%   (1, Info) , [pixels]
+%
+% Maximum_Initial_Potential - The maximum confining potential value that a
+%   particle can have at its initial position. Any possible initial
+%   position with a confining potential larger than this value will not be
+%   used as an initial particle position. 
+%   (-Inf, Inf] , [arb. units]
 %
 % Initial_Speed - The initial speed of the particles in the simulation.  
 %   (-Inf, Inf) , [pixels/time]
@@ -50,18 +52,6 @@ classdef declumpOptions
 % Point_Selection_Method - The method used to initialize the particle
 %   locations.
 %   {'random','uniform','uniformRandom','curvatureRandom','curvatureUniformRandom'}
-%
-% Min_Interior_Angle - The minimum interior angle for the triangulation of
-%   the centers.
-%   [0, 180] , [degrees]
-%
-% Max_Interior_Angle - The maximum interior angle for the triangulation of
-%   the centers.
-%   [0, 180] & > Min_Interior_Angle , [degrees]
-%
-% Search_Radius - The radius of the region over which each cut vertex is
-%   optimized over.
-%   [0, Inf) & integer , [pixels]
 %
 % Minimum_Hole_Size - The minimum hole size allowed in the mask.
 %   [0, Inf) , [pixels^2]
@@ -89,23 +79,18 @@ classdef declumpOptions
 %   this property empty for normal use.
     
     properties
-        Max_Radius                   = 35;
-        Min_Angle                    = 0.5;
-        
+
         Wigner_Seitz_Radius          = 10;
         Potential_Depth
         Potential_Minimum_Location
         Potential_Extent
+        Potential_Padding_Size       = 5;
+        Maximum_Initial_Potential    = 1/3;
         Initial_Speed                = 0.01;
         Particle_Damping_Rate        = 5e-4;
         Charge_Normalization_Beta    = 1/3;
         Solver_Time_Range            = 0:10:1500;
         Point_Selection_Method       = 'curvatureUniformRandom';
-        
-        Min_Interior_Angle           = 20;
-        Max_Interior_Angle           = 110;
-        
-        Search_Radius                = 7;
         
         Minimum_Hole_Size            = 50;
         
@@ -129,13 +114,13 @@ classdef declumpOptions
     end
     
     methods
-        function options = declumpOptions(varargin)
+        function options = seedPointOptions(varargin)
             
             % First get the potential paramters
             try
                 options.potentialParameters = load('potentialParameters.mat');
             catch
-                error('declumpOptions:missingPotentialParamters','The file potentialParameters.mat is missing or not on the path. This file is required for setting the potentialParamters. It may be created with the function computePotentialParamters if you do not have it.')
+                error('seedPointOptions:missingPotentialParamters','The file potentialParameters.mat is missing or not on the path. This file is required for setting the potentialParamters. It may be created with the function computePotentialParamters if you do not have it.')
             end
             
             options.Potential_Depth = options.potentialParameters.depth(1);
@@ -155,23 +140,13 @@ classdef declumpOptions
                     end
                 else
                     if ~mod(nargin+1,2)
-                        error('declumpOptions:badInput','Input must be structure with properties as fiels, or property/value list with even number of elements.')
+                        error('seedPointOptions:badInput','Input must be structure with properties as fields, or property/value list with even number of elements.')
                     end
                     for i = 1:2:numel(varargin)
                         options.(varargin{i}) = varargin{i+1};
                     end
                 end
             end
-        end
-        
-        function obj = set.Max_Radius(obj,value)
-            validateattributes(value,{'double'},{'scalar','positive','real','finite'})
-            obj.Max_Radius = value;
-        end
-        
-        function obj = set.Min_Angle(obj,value)
-            validateattributes(value,{'double'},{'scalar','>=',-1,'<=',1,'real'})
-            obj.Min_Angle = value;
         end
         
         function obj = set.Wigner_Seitz_Radius(obj,value)
@@ -198,7 +173,7 @@ classdef declumpOptions
                 badValue = value;
                 [~, idx] = min(abs(obj.potentialParameters.depth-value));  %#ok<MCSUP>
                 value = obj.potentialParameters.depth(idx);  %#ok<MCSUP>
-                warning('declumpOptions:undefinedPotentialParameterValues', 'There are no potential parameters defined for the Depth %g. Using the closest defined value %g.\nUse the function computePotentialParameters to compute the potential paramters for additional values.',badValue,value)
+                warning('seedPointOptions:undefinedPotentialParameterValues', 'There are no potential parameters defined for the Depth %g. Using the closest defined value %g.\nUse the function computePotentialParameters to compute the potential paramters for additional values.',badValue,value)
             end
             
             obj.potentialParameterIdx(1) = idx; %#ok<MCSUP>
@@ -215,11 +190,11 @@ classdef declumpOptions
                 badValue = value;
                 [~, idx] = min(abs(obj.potentialParameters.center-value));  %#ok<MCSUP>
                 value = obj.potentialParameters.center(idx);  %#ok<MCSUP>
-                warning('declumpOptions:undefinedPotentialParameterValues', 'There are no potential parameters defined for the Minimum_Location %g. Using the closest defined value %g.\nUse the function computePotentialParameters to compute the potential paramters for additional values.',badValue,value)
+                warning('seedPointOptions:undefinedPotentialParameterValues', 'There are no potential parameters defined for the Minimum_Location %g. Using the closest defined value %g.\nUse the function computePotentialParameters to compute the potential paramters for additional values.',badValue,value)
             end
             
             if value > obj.Potential_Extent %#ok<MCSUP>
-                error('declumpOptions:badSet','Potential_Minimum_Location must be smaller than Potential_Extent.')
+                error('seedPointOptions:badSet','Potential_Minimum_Location must be smaller than Potential_Extent.')
             end
             
             obj.potentialParameterIdx(2) = idx; %#ok<MCSUP>
@@ -236,11 +211,11 @@ classdef declumpOptions
                 badValue = value;
                 [~, idx] = min(abs(obj.potentialParameters.extent-value));  %#ok<MCSUP>
                 value = obj.potentialParameters.extent(idx);  %#ok<MCSUP>
-                warning('declumpOptions:undefinedPotentialParameterValues', 'There are no potential parameters defined for the Extent %g. Using the closest defined value %g.\nUse the function computePotentialParameters to compute the potential paramters for additional values.',badValue,value)
+                warning('seedPointOptions:undefinedPotentialParameterValues', 'There are no potential parameters defined for the Extent %g. Using the closest defined value %g.\nUse the function computePotentialParameters to compute the potential paramters for additional values.',badValue,value)
             end
             
             if value < obj.Potential_Minimum_Location %#ok<MCSUP>
-                error('declumpOptions:badSet','Potential_Extent must be larger than Potential_Minimum_Location.')
+                error('seedPointOptions:badSet','Potential_Extent must be larger than Potential_Minimum_Location.')
             end
             
             obj.potentialParameterIdx(3) = idx; %#ok<MCSUP>
@@ -248,25 +223,14 @@ classdef declumpOptions
             obj.Potential_Extent = value;
         end
         
-        function obj = set.Min_Interior_Angle(obj,value)
-            validateattributes(value,{'double'},{'scalar','>=',0,'<=',180})
-            if value > obj.Max_Interior_Angle %#ok<MCSUP>
-                error('declumpOptions:badSet','Min_Interior_Angle must be smaller than Max_Interior_Angle.')
-            end
-            obj.Min_Interior_Angle = value;
+        function obj = set.Potential_Padding_Size(obj,value)
+            validateattributes(value,{'double'},{'integer','scalar','positive','nonzero','real','finite'})
+            obj.Potential_Padding_Size = value;
         end
         
-        function obj = set.Max_Interior_Angle(obj,value)
-            validateattributes(value,{'double'},{'scalar','>=',0,'<=',180})
-            if value < obj.Min_Interior_Angle %#ok<MCSUP>
-                error('declumpOptions:badSet','Max_Interior_Angle must be larger than Min_Interior_Angle.')
-            end
-            obj.Max_Interior_Angle = value;
-        end
-        
-        function obj = set.Search_Radius(obj,value)
-            validateattributes(value,{'double'},{'scalar','integer','nonnegative','real','finite'})
-            obj.Search_Radius = value;
+        function obj = set.Maximum_Initial_Potential(obj,value)
+            validateattributes(value,{'double'},{'scalar','real','>',-Inf,'<=',Inf})
+            obj.Maximum_Initial_Potential = value;
         end
         
         function obj = set.Minimum_Hole_Size(obj,value)
@@ -281,21 +245,21 @@ classdef declumpOptions
         
         function obj = set.Use_GPU(obj,value)
             if (value ~= 0) && (value ~= 1)
-                error('declumpOptions:badInput','Expected input to be logical.')
+                error('seedPointOptions:badInput','Expected input to be logical.')
             end
             obj.Use_GPU = value;
         end
         
         function obj = set.Use_Parallel(obj,value)
             if (value ~= 0) && (value ~= 1)
-                error('declumpOptions:badInput','Expected input to be logical.')
+                error('seedPointOptions:badInput','Expected input to be logical.')
             end
             obj.Use_Parallel = value;
         end
         
         function obj = set.Debug(obj,value)
             if (value ~= 0) && (value ~= 1)
-                error('declumpOptions:badInput','Expected input to be logical.')
+                error('seedPointOptions:badInput','Expected input to be logical.')
             end
             obj.Debug = value;
         end
