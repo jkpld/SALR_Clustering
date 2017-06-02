965e6b6de6b64558ba7da86797af460225bbb253
James Kapaldo <james.kapaldo@gmail.com>
2017-06-02 11:50:25 -0400

    Feat: Add functions for converting between grid and data
    Problem: The previous conversions between grid and data did not include
    the translation of origin between the two spaces, but only the scale
    factors.
    Correction: computeProblemScales() should now recieve the data limits
    and not the data range. The problem_scales structure now has
    data_to_grid and grid_to_data fields that hold function handles to
    convert between the two spaces, and take the Potential_Padding_Size into
    account.
    
    Also changed name of grid_range to grid_size. The size of the grid
    should be given, not the size minus 1.

--- utilities/computeProblemScales.m
+++ utilities/computeProblemScales.m
@@ -1,36 +1,70 @@
-function problem_scales = computeProblemScales(options, grid_range, data_range)
-% COMPUTEPROBLEMSCALES compute the grid spacing and the grid-to-solver
-% scale factor.
+function problem_scales = computeProblemScales(options, grid_size, data_limits)
+% COMPUTEPROBLEMSCALES Compute the scale factors and data transformations
+% for the problem.
 %
-% problem_scales = computeGridScaleFactors(options, grid_range, data_range)
+% problem_scales = computeGridScaleFactors(options, grid_size, data_limits)
 %
-% problem_scales : structure with two fields, grid_spacing and
-% grid_to_solver.
+% Input parameters:
+% options : An instance of class seedPointOptions.
+% grid_size : The size of the grid.
+% data_limits : A 2xD array where the first row gives the minimum values of
+%   the data and the second row gives the maximum values of the data. D is
+%   the dimension of the data.
+%       Ex. If the data is given by an array, dat, where each row is a
+%       point and columns are dimensions, then the data_limits could be
+%       computed using
+%           data_limits = [min(dat,1); max(dat,1)];
+%
+%   *Note: If data_limits is not given, then data_limits will be set to
+%   data_limits = [zeros(1,D), grid_size]. This will give a grid_spacing of
+%   1.
+%
+% Output parameters:
+% problem_scales : structure with four fields
+%   grid_spacing - 1xD array giving the size of each grid bin in terms of
+%       the data. D is the dimension of the data.
+%   data_to_grid - A function handle that converts from data space to grid
+%       space, while also taking the Potential_Padding into account.
+%   grid_to_data - A function handle that is the inverse transform of
+%       data_to_grid.
+%   grid_to_solver - 1xD array giving the conversion factors to go from
+%       grid space to solver space.
+%
+% See also SEEDPOINTOPTIONS
 
 % James Kapaldo
 
-if nargin < 3
-    data_range = [];
-end
-
 % Dimension
-D = length(grid_range);
+D = length(grid_size);
+
+if nargin < 3
+    % If the data limits are not given, assume the grid spacing is 1 and
+    % there is no translation between grid and data spaces. Thus, the data
+    % limits are
+    data_limits = [zeros(1,D); grid_size];
+end
 
 % Compute grid spacing
-if isempty(data_range)
-    % If not set, then assume spacing of 1
-    grid_spacing = ones(1,D);
-else
-    grid_spacing = data_range ./ grid_range;
-end
+%   The grid spacing is used when computing the gradient of the confining
+%   potential.
+data_range = diff(data_limits,1,1);
+grid_spacing = data_range ./ grid_size;
 
-grid_to_data = grid_spacing;
+% Compute grid <--> data conversions.
+PAD_SIZE = options.Potential_Padding_Size;
+data_to_grid = @(r) (r - data_limits(1,:))./grid_spacing + PAD_SIZE;
+grid_to_data = @(r) (r - PAD_SIZE).*grid_spacing + data_limits(1,:);
 
-% Now scale the grid_to_solver space so that the attractive extent is
-% given in terms of the ScaleInvarient_Potential_Extent
+% Compute grid --> solver conversion.
+%   Note that solver space is simply the data space scaled by a factor. The
+%   factor should be such that the attractive extent in solver space is
+%   given by ScaleInvarient_Potential_Extent
 grid_to_solver = grid_spacing * options.ScaleInvarient_Potential_Extent / options.Potential_Parameters(3);
 
 % Form output structure
-problem_scales = struct('grid_spacing', grid_spacing, 'grid_to_data', grid_to_data, 'grid_to_solver', grid_to_solver);
+problem_scales = struct('grid_spacing', grid_spacing, ...
+                        'data_to_grid', data_to_grid, ...
+                        'grid_to_data', grid_to_data, ...
+                        'grid_to_solver', grid_to_solver);
 
 end
