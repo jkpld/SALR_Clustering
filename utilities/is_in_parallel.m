bdcd9cd321a1a7dfe58560f580a532dc7ffc7bcd
James Kapaldo <james.kapaldo@gmail.com>
2017-06-02 15:48:25 -0400

    Feat: Add ability to determine if function is running in parallel

---
+++ utilities/is_in_parallel.m
@@ -0,0 +1,14 @@
+function answer = is_in_parallel()
+% IS_IN_PARALLEL Determine if function is being run in parallel.
+
+% https://www.mathworks.com/matlabcentral/answers/58228-am-i-running-in-parallel-best-way-to-check
+
+    try
+        answer = ~isempty(getCurrentTask());
+    catch err
+        if ~strcmp(err.identifier, 'MATLAB:UndefinedFunction')
+            rethrow(err);
+        end
+        answer = false;
+    end
+end
