eda086c5b50d7479c0b911b1cfcb012708c36053
James Kapaldo <james.kapaldo@gmail.com>
2017-06-03 19:08:32 -0400

    Refactor: rename function

--- utilities/get_minimum_uint_class.m
+++ utilities/get_minimum_uint_class.m
@@ -1,4 +1,4 @@
-function class_name = get_minimum_uint_size(A)
+function class_name = get_minimum_uint_class(A)
 
 maxValue = max(A(:));
 
@@ -7,7 +7,7 @@ if maxValue <= intmax('uint8')
 elseif maxValue <= intmax('uint16')
 	class_name = 'uint16';
 elseif maxValue <= intmax('uint32')
-	class_name = 'uint32';	
+	class_name = 'uint32';
 elseif maxValue <= intmax('uint64')
 	class_name = 'uint64';
 elseif mod(maxValue,1) ~= 0
