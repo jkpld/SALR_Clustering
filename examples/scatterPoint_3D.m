a05e9a75249726ceb51469ff9339b547f286ba53
James Kapaldo <james.kapaldo@gmail.com>
2017-06-04 00:11:58 -0400

    Update:
    * Use computeObjectSeedPoints
    * Use new parameters for create_3d_density_plot
    * Update documentation and style a bit

--- examples/scatterPoint_3D.m
+++ examples/scatterPoint_3D.m
@@ -5,115 +5,110 @@
 %% Load data
 dat = create_test_3D_object(80000);
 
-% Scale data : the test nuclei are a ~5 times smaller in hight than diameter.
+% Scale data: In order to use the distance transform each object should be
+% about the size size along any dimension. The objects in this data set are
+% about 5 times smaller in height than their latteral size; so, scale the
+% heights by 5.
 dat = dat .* [1, 1, 5];
-%% Compute data density
+
+
+%% Bin the data
 nbins = 30;
 density_threshold = 2;
-smooth_data = true; % use a faster smoothing after preperation
-
-[n, cents, sz] = binData(dat, nbins, density_threshold, smooth_data, true);
-
-% n = frequencyGaussianFilter(n,0.5,3,'replicate');
-% n(n<0) = 0;
-
-% Get data scale factors
-data_range = cellfun(@(x) range(x), cents);
-data_scale = data_range./(sz-1);
+smooth_data = true;
+[n, cents] = binData(dat, nbins, density_threshold, smooth_data, true);
 
 %% Explore/Visualize the data
-
 dimensions = [1,2,3];
-isoLevels = [10,20,30,40];%[2,5,8,11];
+isoLevels = [10,20,30,40];
+tick_spacing = [30,30,20];
 
-ticks = cell(1,3);
-ticklbls = cell(1,3);
-
-dt = [40,40,20];
-for i = 1:3
-    c = cents{dimensions(i)};
-    c1 = fix(c(1));
-    c2 = fix(c(end));
-    
-    ticklbls{i} = round(c1/dt(i))*dt(i):dt(i):(c2-dt(i)/2);
-    ticks{i} = interp1(c, 1:sz(dimensions(i)), ticklbls{i});
-end
-ticklbls{3} = ticklbls{3};
-color_scale = @(x) x;
-
-% try close(fig), catch, end;
-fig = create_3d_density_plot(n, dimensions, isoLevels,'ColorScale',@(x)(x),'Ticks',ticks,'TickLabels',ticklbls);
+fig = create_3d_density_plot(n, dimensions, isoLevels, ...
+    'bin_centers', cents, 'tick_spacing', tick_spacing);
 
 set(gca,'XDir','reverse')
 view(220,31)
-
 
 %% Create mask
 Omega = n > 5;
 
 %% Locate seed-points using distance transform based confining potential
-% This section should be modified. the data should be scaled better so that
-% the hights are about the radii. 
-
 options = seedPointOptions();
-options.Wigner_Seitz_Radius = 2.5; % This is in units of the discretized space!
-options.Wigner_Seitz_Radius_Space = 'grid';
-options.Point_Selection_Method = 'uniformRandom';
-options.Maximum_Initial_Potential = 1/5;
-options.Potential_Padding_Size = 1;
 
-% Scale the object so that the maximum distance transform value is 18.
-options.Max_Distance_Transform = 18;
+% Set the particle initialization parameters
+options.Point_Selection_Method = 'uniformRandom';
+options.Wigner_Seitz_Radius = 2.5;
+options.Wigner_Seitz_Radius_Space = 'grid';
+options.Maximum_Initial_Potential = 1/5;
 
 % Set the particle interaction parameter values.
 options.Potential_Parameters = [-1, 2, 13];
-options.Potential_Parameters_Space = 'solver'; % No
 
-% options.Particle_Damping_Rate = 5e-4;
+% Scale the object so that the maximum distance transform value is 18.
+options.Max_Distance_Transform = 18;
+options.Potential_Padding_Size = 2;
+
+% Set up replicates and minimum cluster size.
+options.Iterations = 10;
+options.Minimum_Cluster_Size = 10/3;
+
+options.Verbose = true;
 options.Debug = true;
 options.Use_Parallel = false;
 
+% Repeat process NN times to check reproducability.
 NN = 1;
 seedPoints = cell(NN,1);
-tmp_seedPoints = [];
-for i = 1:NN
-N = 20;
-start = tic;
+seedPoints_n = [];
 
-[seedPoints{i},Info] = compute_seedPoints_nd(Omega, data_range, options,'iterations',N,'minClusterSize',N/2,'verbose',0);
-tmp_seedPoints = [tmp_seedPoints; Info.seedPoint_set];
-fprintf('Finished, avg set time = %f\n', toc(start)/N)
+for i = 1:NN
+    start = tic;
+
+    % Compute the seed points
+    [seedPoints{i},Info] = computeObjectSeedPoints(Omega, options,'data_limits',data_limits);
+
+    % Save the seed point results of each replicate
+    seedPoints_n = [seedPoints_n; Info.seedPoints_n]; %#ok<AGROW>
+
+    fprintf('Finished, avg set time = %f\n', toc(start)/options.Iterations)
 end
 
-
-seedPoint_set_all = cat(1,seedPoints{:});
-seedPoint_set = tmp_seedPoints;%Info.seedPoint_set;
-r0 = Info.iteration_info{1}.r0;
+seedPoints = cat(1,seedPoints{:});
 
 % Plot the results
-options.Potential_Padding_Size = 0;
-V = create_scaleInvar_confining_potential(Omega,options);
+V = removePadding(Info.V, options.Potential_Padding_Size);
+
 dimensions = [1,2,3];
 isoLevels = [1,5,10,15];
 markers = [];
 
-markers(2).dat = Info.data_to_grid(seedPoint_set_all);
+% Helper function to convert are seed points into grid space
+data_to_grid = @(t) Info.problem_scales.data_to_grid(t) - ...
+    options.Potential_Padding_Size;
+
+% Plot the final seed points as large red dots
+markers(2).dat = data_to_grid(seedPoints);
 markers(2).options.Marker = '.';
 markers(2).options.Color = 'r';
 markers(2).options.LineStyle = 'none';
 markers(2).options.MarkerSize = 15;
 markers(2).project = true;
 
-markers(1).dat = Info.data_to_grid(seedPoint_set);
+% Plot the seed points of each replicate as small black dots
+markers(1).dat = data_to_grid(seedPoints_n);
 markers(1).options.Marker = '.';
 markers(1).options.Color = 'k';
 markers(1).options.LineStyle = 'none';
 markers(1).options.MarkerSize = 4;
 markers(1).project = true;
 
-
 try close(fig), catch, end
-fig = create_3d_density_plot(1./V, dimensions, isoLevels,'ColorScale',@(x)sqrt(x), 'Markers',markers,'Ticks',ticks,'TickLabels',ticklbls);
+fig = create_3d_density_plot(1./V, dimensions, isoLevels, ...
+    'ColorScale', @(x)sqrt(x), ...
+    'Markers', markers, ...
+    'bin_centers', cents, ...
+    'tick_spacing', tick_spacing);
+
 set(gca,'XDir','reverse')
 view(220,31)
 
@@ -141,21 +136,24 @@ dimensions = [1,2,3];
 isoLevels = 10:10:40;
 
 markers = [];
-
 markers(1).dat = c;
 markers(1).options.Marker = '.';
 markers(1).options.Color = 'r';
 markers(1).options.LineStyle = 'none';
 markers(1).options.MarkerSize = 20;
 markers(1).project = false;
-%
+
 try close(fig), catch, end
-fig = create_3d_density_plot(n, dimensions, isoLevels, 'Markers', markers, 'ColorScale', @(x) (x),'Ticks',ticks,'TickLabels',ticklbls);
+fig = create_3d_density_plot(n, dimensions, isoLevels, ...
+    'Markers', markers, ...
+    'bin_centers', cents, ...
+    'tick_spacing', tick_spacing);
+
 set(gca,'XDir','reverse')
 view([180,90])
 %% Export figure
 % Export lines as pdf.
-% Export transparent isosurfaces as png. 
+% Export transparent isosurfaces as png.
 % Overlay the png on the pdf in other software.
 %
 % Note, this is distructive! It will delete the transparent objects before
