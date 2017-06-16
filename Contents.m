a41110a05d921310acb8d4738f7a75b1d2436d6c
James Kapaldo <james.kapaldo@gmail.com>
2017-06-16 00:35:40 -0400

    Remove old functions/files

--- Contents.m
+++
@@ -1,6 +0,0 @@
-% DECLUMPNUCLEI
-%
-% Files
-%   computeObjectCenters - Compute object centers by particle simulation
-%   declumpNuclei        - Declump the nuclei in an image
-%   declumpOptions       - Set options needed for declumping.
