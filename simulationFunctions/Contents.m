cf31e070f4a36fd1d48ba6ff6b13284e6dbc68ab
James Kapaldo <james.kapaldo@gmail.com>
2017-06-16 00:20:01 -0400

    Refactor: Move functions around

--- simulationFunctions/Contents.m
+++
@@ -1,6 +0,0 @@
-% SIMULATIONFUNCTIONS
-%
-% Files
-%   computeInitialPoints                    - Generate a set of initial particle locations for
-%   interactingParticleSystem               - Given particle position (r) and momentum (p)
-%   interactingParticleSystem_convergeEvent - Determine if the particle system
